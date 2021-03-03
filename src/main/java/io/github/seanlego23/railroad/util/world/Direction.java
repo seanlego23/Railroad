801abd8f06aa110a936b5ee00d3c8e91448578d8
seanlego23 <seanlego23@gmail.com>
2021-03-02 23:11:09 -0500

    Added method to get Direction from a Vector

--- src/main/java/io/github/seanlego23/railroad/util/world/Direction.java
+++ src/main/java/io/github/seanlego23/railroad/util/world/Direction.java
@@ -2,19 +2,70 @@ package io.github.seanlego23.railroad.util.world;
 
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Direction {
-	SOUTH(0, 1),
-	NORTH(0, -1),
-	EAST(1, 0),
-	WEST(-1, 0);
+	SOUTH(0, 0, 1),
+	NORTH(0, 0, -1),
+	EAST(1, 0, 0),
+	WEST(-1, 0, 0),
+	UP(0, 1,0),
+	DOWN(0, -1,0);
 
-	private Vector vector;
+	private final Vector vector;
 
-	Direction(int x, int z) {
-		this.vector = new Vector(x, 0, z);
+	Direction(int x, int y, int z) {
+		this.vector = new Vector(x, y, z);
 	}
 
 	public Vector getVector() {
-		return this.vector;
+		return this.vector.clone();
+	}
+
+	//If in between two directions:
+	//UP and DOWN are top priority
+	//otherwise SOUTH < EAST < NORTH < WEST < SOUTH
+	//Essentially -pi < x <= pi
+	public static Direction getFromVector(Vector vector) {
+		Vector other = vector.clone().normalize();
+		List<Direction> inBetween = new ArrayList<>();
+		for (Direction direction : Direction.values()) {
+			if (direction.getVector().angle(other) < (float)(Math.PI / 4))
+				return direction;
+			else if (direction.getVector().angle(other) == (float)(Math.PI / 4))
+				inBetween.add(direction);
+		}
+
+		if (inBetween.size() == 1)
+			return inBetween.get(0);
+
+		Direction first = inBetween.get(0);
+		Direction second = inBetween.get(1);
+		if (first == UP || second == UP)
+			return UP;
+		else if (first == DOWN || second == DOWN)
+			return DOWN;
+		else if (first == WEST) {
+			if (second == SOUTH)
+				return second;
+			else
+				return first;
+		} else if (first == SOUTH) {
+			if (second == EAST)
+				return second;
+			else
+				return first;
+		} else if (first == EAST) {
+			if (second == NORTH)
+				return second;
+			else
+				return first;
+		} else {
+			if (second == WEST)
+				return second;
+			else
+				return first;
+		}
 	}
 }
