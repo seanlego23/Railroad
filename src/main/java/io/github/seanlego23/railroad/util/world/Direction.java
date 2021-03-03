ec0b6fb860a9a924996c3144702536d0142c0e28
seanlego23 <seanlego23@gmail.com>
2021-03-02 21:06:46 -0500

    Configured for Vector translation

--- src/main/java/io/github/seanlego23/railroad/util/world/Direction.java
+++ src/main/java/io/github/seanlego23/railroad/util/world/Direction.java
@@ -1,8 +1,20 @@
 package io.github.seanlego23.railroad.util.world;
 
+import org.bukkit.util.Vector;
+
 public enum Direction {
-	SOUTH,
-	NORTH,
-	EAST,
-	WEST
+	SOUTH(0, 1),
+	NORTH(0, -1),
+	EAST(1, 0),
+	WEST(-1, 0);
+
+	private Vector vector;
+
+	Direction(int x, int z) {
+		this.vector = new Vector(x, 0, z);
+	}
+
+	public Vector getVector() {
+		return this.vector;
+	}
 }
