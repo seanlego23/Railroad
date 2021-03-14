d504b2f3eb1c5cbfe88957e7c3fa1d92aa6a0e98
seanlego23 <seanlego23@gmail.com>
2021-03-14 16:10:06 -0400

    Added method getID

--- src/main/java/io/github/seanlego23/railroad/track/ITrackStop.java
+++ src/main/java/io/github/seanlego23/railroad/track/ITrackStop.java
@@ -1,8 +1,12 @@
 package io.github.seanlego23.railroad.track;
 
+import io.github.seanlego23.railroad.util.target.RUID;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * The ITrackStop interface is just a marker interface to denote that an ITrack
  * can connect to an class that implements this interface.
  */
 public interface ITrackStop {
+    @NotNull RUID getID();
 }
