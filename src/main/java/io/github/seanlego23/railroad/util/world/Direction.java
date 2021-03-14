41b90580be4b0f7dd18a9a99bdd0347c99db01e7
seanlego23 <seanlego23@gmail.com>
2021-03-14 14:18:33 -0400

    Expanded the usability of Direction

--- src/main/java/io/github/seanlego23/railroad/util/world/Direction.java
+++ src/main/java/io/github/seanlego23/railroad/util/world/Direction.java
@@ -2,16 +2,34 @@ package io.github.seanlego23.railroad.util.world;
 
 import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public enum Direction {
 	SOUTH(0, 0, 1),
+	SOUTH_UP(0, 1, 1),
+	SOUTH_DOWN(0, -1, 1),
 	NORTH(0, 0, -1),
+	NORTH_UP(0, 1, -1),
+	NORTH_DOWN(0, -1, -1),
 	EAST(1, 0, 0),
+	EAST_UP(1, 1, 0),
+	EAST_DOWN(1, -1, 0),
 	WEST(-1, 0, 0),
+	WEST_UP(-1, 1, 0),
+	WEST_DOWN(-1, -1, 0),
+	SOUTH_EAST(1, 0, 1),
+	SOUTH_EAST_UP(1, 1, 1),
+	SOUTH_EAST_DOWN(1, -1, 1),
+	SOUTH_WEST(-1, 0, 1),
+	SOUTH_WEST_UP(-1, 1, 1),
+	SOUTH_WEST_DOWN(-1, -1, 1),
+	NORTH_EAST(1, 0, -1),
+	NORTH_EAST_UP(1, 1, -1),
+	NORTH_EAST_DOWN(1, -1, -1),
+	NORTH_WEST(-1, 0, -1),
+	NORTH_WEST_UP(-1, 1, -1),
+	NORTH_WEST_DOWN(-1, -1, -1),
 	UP(0, 1,0),
-	DOWN(0, -1,0);
+	DOWN(0, -1,0),
+	HERE(0,0,0);
 
 	private final Vector vector;
 
@@ -27,62 +45,135 @@ public enum Direction {
 		switch (this) {
 			case SOUTH:
 				return NORTH;
+			case SOUTH_UP:
+				return NORTH_DOWN;
+			case SOUTH_DOWN:
+				return NORTH_UP;
 			case NORTH:
 				return SOUTH;
+			case NORTH_UP:
+				return SOUTH_DOWN;
+			case NORTH_DOWN:
+				return SOUTH_UP;
 			case EAST:
 				return WEST;
+			case EAST_UP:
+				return WEST_DOWN;
+			case EAST_DOWN:
+				return WEST_UP;
 			case WEST:
 				return EAST;
+			case WEST_UP:
+				return EAST_DOWN;
+			case WEST_DOWN:
+				return EAST_UP;
+			case SOUTH_EAST:
+				return NORTH_WEST;
+			case SOUTH_EAST_UP:
+				return NORTH_WEST_DOWN;
+			case SOUTH_EAST_DOWN:
+				return NORTH_WEST_UP;
+			case SOUTH_WEST:
+				return NORTH_EAST;
+			case SOUTH_WEST_UP:
+				return NORTH_EAST_DOWN;
+			case SOUTH_WEST_DOWN:
+				return NORTH_EAST_UP;
+			case NORTH_EAST:
+				return SOUTH_WEST;
+			case NORTH_EAST_UP:
+				return SOUTH_WEST_DOWN;
+			case NORTH_EAST_DOWN:
+				return SOUTH_WEST_UP;
+			case NORTH_WEST:
+				return SOUTH_EAST;
+			case NORTH_WEST_UP:
+				return SOUTH_EAST_DOWN;
+			case NORTH_WEST_DOWN:
+				return SOUTH_EAST_UP;
 			case UP:
 				return DOWN;
-			default:
+			case DOWN:
 				return UP;
+			default:
+				return this;
 		}
 	}
 
-	//If in between two directions:
-	//UP and DOWN are top priority
-	//otherwise SOUTH < EAST < NORTH < WEST < SOUTH
-	//Essentially -pi < x <= pi
-	public static Direction getFromVector(Vector vector) {
-		Vector other = vector.clone().normalize();
-		List<Direction> inBetween = new ArrayList<>();
-		for (Direction direction : Direction.values()) {
-			if (direction.getVector().angle(other) < (float)(Math.PI / 4))
-				return direction;
-			else if (direction.getVector().angle(other) == (float)(Math.PI / 4))
-				inBetween.add(direction);
+	public Direction getRegular() {
+		switch (this) {
+			case SOUTH:
+			case SOUTH_UP:
+			case SOUTH_DOWN:
+				return SOUTH;
+			case NORTH:
+			case NORTH_UP:
+			case NORTH_DOWN:
+				return NORTH;
+			case EAST:
+			case EAST_UP:
+			case EAST_DOWN:
+				return EAST;
+			case WEST:
+			case WEST_UP:
+			case WEST_DOWN:
+				return WEST;
+			case SOUTH_EAST:
+			case SOUTH_EAST_UP:
+			case SOUTH_EAST_DOWN:
+				return SOUTH_EAST;
+			case SOUTH_WEST:
+			case SOUTH_WEST_UP:
+			case SOUTH_WEST_DOWN:
+				return SOUTH_WEST;
+			case NORTH_EAST:
+			case NORTH_EAST_UP:
+			case NORTH_EAST_DOWN:
+				return NORTH_EAST;
+			case NORTH_WEST:
+			case NORTH_WEST_UP:
+			case NORTH_WEST_DOWN:
+				return NORTH_WEST;
+			default:
+				return this;
 		}
+	}
 
-		if (inBetween.size() == 1)
-			return inBetween.get(0);
+	public boolean onZAxis() {
+		switch (this) {
+			case SOUTH:
+			case SOUTH_UP:
+			case SOUTH_DOWN:
+			case NORTH:
+			case NORTH_UP:
+			case NORTH_DOWN:
+				return true;
+			default:
+				return false;
+		}
+	}
 
-		Direction first = inBetween.get(0);
-		Direction second = inBetween.get(1);
-		if (first == UP || second == UP)
-			return UP;
-		else if (first == DOWN || second == DOWN)
-			return DOWN;
-		else if (first == WEST) {
-			if (second == SOUTH)
-				return second;
-			else
-				return first;
-		} else if (first == SOUTH) {
-			if (second == EAST)
-				return second;
-			else
-				return first;
-		} else if (first == EAST) {
-			if (second == NORTH)
-				return second;
-			else
-				return first;
-		} else {
-			if (second == WEST)
-				return second;
-			else
-				return first;
+	public boolean onXAxis() {
+		switch (this) {
+			case EAST:
+			case EAST_UP:
+			case EAST_DOWN:
+			case WEST:
+			case WEST_UP:
+			case WEST_DOWN:
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	public boolean onYAxis() {
+		switch (this) {
+			case UP:
+			case DOWN:
+				return true;
+			default:
+				return false;
 		}
 	}
 }
