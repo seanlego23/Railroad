909df7a59c1a148c5270e2b8c9050a8bff2d26e1
seanlego23 <seanlego23@gmail.com>
2021-03-15 18:04:22 -0400

    Can now find ITrack's and Junctions via RUID

--- src/main/java/io/github/seanlego23/railroad/track/TrackManager.java
+++ src/main/java/io/github/seanlego23/railroad/track/TrackManager.java
@@ -1,5 +1,7 @@
 package io.github.seanlego23.railroad.track;
 
+import io.github.seanlego23.railroad.util.target.IncorrectRUIDTypeException;
+import io.github.seanlego23.railroad.util.target.RUID;
 import io.github.seanlego23.railroad.world.World;
 import org.bukkit.Location;
 
@@ -17,7 +19,30 @@ public class TrackManager {
 		return null;
 	}
 
+	public static ITrack getTrack(RUID ruid) throws IncorrectRUIDTypeException {
+		if (ruid.getType() != RUID.IDType.TRACK)
+			throw new IncorrectRUIDTypeException("RUID is not a ITrack RUID.");
+		return null;
+	}
+
 	public ITrack getCachedTrack(Location location) {
+		return null;
+	}
+
+	public Junction getJunction(Location location) {
+		Junction junction = this.getCachedJunction(location);
+		if (junction != null)
+			return junction;
+		return null;
+	}
+
+	public static Junction getJunction(RUID ruid) throws IncorrectRUIDTypeException {
+		if (ruid.getType() != RUID.IDType.JUNCTION)
+			throw new IncorrectRUIDTypeException("RUID is not a Junction RUID.");
+		return null;
+	}
+
+	public Junction getCachedJunction(Location location) {
 		return null;
 	}
 
