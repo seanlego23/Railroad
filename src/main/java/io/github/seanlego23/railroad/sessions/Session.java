addea321a1aeb109f3654696802e1f6ef083c18d
seanlego23 <seanlego23@gmail.com>
2021-03-14 20:51:58 -0400

    Fixed creation and added deletion of LocalSession's

--- src/main/java/io/github/seanlego23/railroad/sessions/Session.java
+++ src/main/java/io/github/seanlego23/railroad/sessions/Session.java
@@ -1,10 +1,8 @@
 package io.github.seanlego23.railroad.sessions;
 
-import io.github.seanlego23.railroad.stations.selectionmethod.SelectionObject;
 import io.github.seanlego23.railroad.user.User;
 import io.github.seanlego23.railroad.world.World;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,14 +20,18 @@ public class Session {
 	}
 
 	public LocalSession createLocalSession(@NotNull World world) {
-		return this.createLocalSession(world, null);
-	}
-
-	public LocalSession createLocalSession(@NotNull World world, @Nullable SelectionObject object) {
 		if (this.localSessionMap.containsKey(world))
 			return this.localSessionMap.get(world);
-		LocalSession newLS = new LocalSession(world, this.user, object);
+		LocalSession newLS = new LocalSession(world, this.user);
 		this.localSessionMap.put(world, newLS);
 		return newLS;
+	}
+
+	public boolean removeLocalSession(@NotNull World world) {
+		return this.localSessionMap.remove(world) != null;
+	}
+
+	public void removeAllLocalSessions() {
+		this.localSessionMap.clear();
 	}
 }
